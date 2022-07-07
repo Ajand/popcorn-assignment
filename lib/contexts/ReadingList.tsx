db36e615e03192b5d91ad504baa4712125d081bc
Ajand Mardalizad <aseedgeek@gmail.com>
2022-07-07 20:24:43 +0430

    added presistance with localstorage

--- lib/contexts/ReadingList.tsx
+++ lib/contexts/ReadingList.tsx
@@ -1,4 +1,4 @@
-import { useContext, createContext, useState, ReactNode, FC } from "react";
+import { createContext, useState, ReactNode, FC, useEffect } from "react";
 import { Book } from "../types";
 
 interface ReadingList {
@@ -15,20 +15,37 @@ export const ReadingListContext = createContext<ReadingList>({
   isInReadingList: (bookId) => false,
 });
 
+const mapToString = (mapping: Map<any, any>) => {
+  return JSON.stringify(Array.from(mapping));
+};
+
+const stringToMap = (target: string): Map<string, Book> => {
+  return new Map(JSON.parse(target));
+};
+
 export const ReadingListProvider: FC<{ children: ReactNode }> = ({
   children,
 }) => {
   const [readingList, setReadingList] = useState<Map<string, Book>>(new Map());
 
+  useEffect(() => {
+    const cachedData = localStorage.getItem("readingList");
+    setReadingList(stringToMap(cachedData ? cachedData : "[]"));
+  }, []);
+
   const addToReadingList = (book: Book) => {
     const currReadingList = new Map(readingList);
     currReadingList.set(String(book.id), book);
+    const currReadingListString = mapToString(currReadingList);
+    localStorage.setItem("readingList", currReadingListString);
     setReadingList(currReadingList);
   };
 
   const deleteFromReadingList = (bookId: string) => {
     const currReadingList = new Map(readingList);
     currReadingList.delete(bookId);
+    const currReadingListString = mapToString(currReadingList);
+    localStorage.setItem("readingList", currReadingListString);
     setReadingList(currReadingList);
   };
 
